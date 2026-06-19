b6ec4af50137ada69dee43427a30e470d63c9da5
amandakoster <amandakoster@gmail.com>
2026-06-19 13:51:20 -0400

    chnage1things testcommit for deploy

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -6,4 +6,5 @@ const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 if (!url || !key) {
   throw new Error("Missing Supabase environment variables");
 }
+
 export const supabase = createClient(url, key);
