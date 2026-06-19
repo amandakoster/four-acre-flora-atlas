9f28d924176279758ff51db16cc36b425b764f1d
amandakoster <amandakoster@gmail.com>
2026-06-19 14:59:09 -0400

    adds use clinet

--- app/observations/page.tsx
+++ app/observations/page.tsx
@@ -1,10 +1,13 @@
+"use client";
+
+"use client";
+
 import AddObservationForm from "@/components/forms/AddObservationForm";
 
 export default function ObservationsPage() {
   return (
     <main className="p-10">
       <h1 className="mb-8 text-5xl font-bold">Add Observation</h1>
-
       <AddObservationForm />
     </main>
   );
